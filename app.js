5ff7df07123e1ab9feba45a164c6d95aba55aacc
Steve Hull <hullstevenj@gmail.com>
2025-03-31 11:29:13 -0700

    Created layouts dir, moved layout.pug to layouts and renamed to main-layout.pug. Added a helper function in app.js for aliasing views. Changes to main-layout.pug for aliasing to partials

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ const app = express();
 
 const indexRouter = require('./routes/index');
 
+// Set Pug as the view engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// Add a helper function to make path aliases work
+app.locals.basedir = path.join(__dirname, 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(indexRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {title: 'Page Not Found'});
 });
 
 app.listen(PORT);
