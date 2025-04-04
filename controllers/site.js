de6cf0787d0a3078817b347aed852c79db3d02b2
Steve Hull <hullstevenj@gmail.com>
2025-04-04 10:24:34 -0700

    Created faqs.pug and refactored header.pug links

--- controllers/site.js
+++ controllers/site.js
@@ -1,3 +1,7 @@
 exports.getIndex = (req, res, next) => {
     res.render('index', { title: 'Welcome to Kenpokicks '});
 };
+
+exports.getFaqs = (req, res, next) => {
+    res.render('faqs', {title: 'Frequently Asked Questions' });
+}
