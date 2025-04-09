0db400c3ef4911682e32a518a8b3a99e5cbd7221
Steve Hull <hullstevenj@gmail.com>
2025-04-08 20:49:24 -0700

    Refactor controllers/error.js to add get500 function

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,21 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {title: 'Page Not Found'});
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        statusCode: 404,
+        message: "The page you're looking for does not exist.",
+        error: null,
+        showStack: false
+    
+    });
+};
+
+exports.get500 = (err, req, res, next) => {
+    console.error('Server Error', err.stack);
+    res.status(500).render('error', {
+        pageTitle: 'Server Error',
+        statusCode: 500,
+        message: 'An unexpected error occurred',
+        error: err,
+        showStack: process.env.NODE_END !== 'production'
+    });
 };
