e4424a1d52ed99d1b89120c80603480b3072fc2b
Steve Hull <hullstevenj@gmail.com>
2025-04-10 20:58:31 -0700

    Refactor siteController.js for showHomeLink status

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -4,14 +4,16 @@ const siteModel = require('../models/siteModel');
 
 exports.getIndex = (req, res, next) => {
     res.render('index', { 
-        pageTitle: 'Welcome to Kenpokicks '
+        pageTitle: 'Welcome to Kenpokicks ',
+        showHomeLink: false
     });
 };
 
 exports.getHistory = (req, res, next) => {
     res.render('history', { 
         pageTitle: 'History of Kenpo',
-        mainWidthClass: 'max-w-4x1'
+        mainWidthClass: 'max-w-4x1',
+        showHomeLink: true
     });
 };
 
@@ -22,7 +24,8 @@ exports.getFaqs = (req, res, next) => {
         res.render('faqs', {
             pageTitle: 'Frequently Asked Questions',
             faqs,
-            mainWidthClass: 'max-w-4x1'
+            mainWidthClass: 'max-w-4x1',
+            showHomeLink: true
         });
     } catch (err) {
         console.error('Error reading faqs:', err);
