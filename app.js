7cb669fc12aae973d771962aae2ec185117548dd
Steve Hull <hullstevenj@gmail.com>
2025-06-20 13:51:57 -0700

    Refactor app.js to change indexRouter to publicRouter

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const indexRouter = require('./routes/indexRoutes');
+const publicRouter = require('./routes/publicRoutes');
 
 // Set Pug as the view engine
 app.set('view engine', 'pug');
@@ -18,7 +18,7 @@ app.locals.basedir = path.join(__dirname, 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(indexRouter);
+app.use(publicRouter);
 
 app.use(errorController.get500);
 app.use(errorController.get404);
