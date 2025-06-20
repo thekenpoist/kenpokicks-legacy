1b1e72a8feec911e2596a2573f14f1717add97ac
Steve Hull <hullstevenj@gmail.com>
2025-06-20 11:23:17 -0700

    Add route in indexRoutes to getPhilosophy in siteController

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', siteController.getIndex);
 router.get('/faqs', siteController.getFaqs);
 router.get('/history', siteController.getHistory);
+router.get('/philosophy', siteController.getPhilosophy);
 
 module.exports = router;
