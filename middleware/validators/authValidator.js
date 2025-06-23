b77ee5c810a068a0c884079b6a571d6c10ae2b31
Steve Hull <hullstevenj@gmail.com>
2025-06-23 16:02:35 -0700

    Minor enhancements to signupRules in authValidator

--- middleware/validators/authValidator.js
+++ middleware/validators/authValidator.js
@@ -12,10 +12,28 @@ exports.loginRules = [
 ];
 
 exports.signupRules = [
+    body('firstName')
+        .isLength({ max: 50 })
+        .notEmpty().withMessage('First name is required.')
+        .trim(),
+
+    body('lastName')
+        .isLength({ max: 50 })
+        .notEmpty().withMessage('Last name is required.')
+        .trim(),
+
+    body('username')
+        .toLowerCase()
+        .notEmpty().withMessage('Username is required.')
+        .isLength({ min: 8, max: 25 }).withMessage('Username must be between 3 and 25 characters.')
+        .matches(/^[a-zA-Z0-9_\-]+$/).withMessage('Username may only contain letters, numbers, underscores, or dashes.')
+        .trim(),
+
     body('email')
         .isEmail()
         .withMessage('A valid email is required.')
         .normalizeEmail(),
+
     body('confirmEmail')
         .custom((value, { req }) => {
             if (value !== req.body.email) {
@@ -23,11 +41,13 @@ exports.signupRules = [
             }
             return true;
         }),
+
     body('password')
         .isLength({ min: 10, max: 25 })
         .withMessage('Password must be between 10 and 25 characters.')
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{10,25}$/)
         .withMessage('Password must include upper/lowercase, number, and symbol.'),
+
     body('confirmPassword')
         .custom((value, { req }) => {
             if (value !== req.body.password) {
