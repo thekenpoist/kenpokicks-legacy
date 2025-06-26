5c2c57ab369e407ac3a79e10c8c6b810fe3eb37b
Steve Hull <hullstevenj@gmail.com>
2025-06-26 15:07:29 -0700

    Rename uploadAvatar to uploadAvatarMiddleware and refactor affected files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const { body } = require('express-validator');
 const { signupRules, loginRules } = require('../middleware/validators/authValidator');
-const processAvatar = require('../middleware/uploadAvatar');
+const processAvatar = require('../middleware/uploadAvatarMiddleware');
 
 const authController = require('../controllers/authController');
 
