2c955e8a9f3d535137713bd00695b133cd9e0bdc
Steve Hull <hullstevenj@gmail.com>
2025-06-27 15:31:58 -0700

    Refactor userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ const path = require('path');
 const express = require('express');
 const { body } = require('express-validator');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
-const { editUserRules, updateSettingsRules } = require('../middleware/validators/userValidator')
+const { validateProfileUpdate } = require('../middleware/validators/userValidator')
+const processAvatar = require('../middleware/uploadAvatarMiddleware');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/edit-profile', isAuthenticated, userController.getEditProfile);
-router.post('/edit-profile', isAuthenticated, editUserRules, updateSettingsRules, userController.postEditProfile);
+router.post('/edit-profile', isAuthenticated, processAvatar, validateProfileUpdate, userController.postEditProfile);
 
 module.exports = router;
