8be3eb558196bb95289f519d4fa605335c3a52d4
Steve Hull <hullstevenj@gmail.com>
2025-06-28 08:49:49 -0700

    Create utils/sessionUtil.js to handle redirect for auto logging out after email or password change

---
+++ utils/sessionUtil.js
@@ -0,0 +1,23 @@
+const logger = require('../utils/loggerUtil');
+
+function logoutAndRedirect(req, res, redirectPath, queryParam = '') {
+    req,session.destroy(err => {
+        if (err) {
+            logger.error(`Error destroying session: ${err.message}`);
+            if (err.stack) {
+                logger.error(err.stack);
+            }
+            return res.redirect(redirectPath);
+        }
+
+        const separator = redirectPath.includes('?') ? '&' : '?';
+        const fullRedirect = queryParam
+            ? `${redirectPath}${separator}${queryParam}`
+            : redirectPath;
+
+        res.clearCookie('connect.sid');
+        res.redirect(fullRedirect);
+    });
+}
+
+module.exports = { logoutAndRedirect };
