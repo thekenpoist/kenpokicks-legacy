3e800c65cbb623a54698164e123b14437ec2162b
Steve Hull <hullstevenj@gmail.com>
2025-06-30 16:30:16 -0700

    Create trainingRoutes.js

---
+++ routes/trainingRoutes.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const trainingController = require('../controllers/trainingController');
+const { isAuthenticated } = require('../middleware/auth/authMiddleware');
+
+const router = express.Router();
+
+router.get('/:beltColor/curriculum', isAuthenticated, trainingController.getCurriculum);
+
+module.exports = router;
