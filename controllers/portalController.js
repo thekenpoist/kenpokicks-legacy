c4d71baccb9cc9e7d84971504a2d38998af04a35
Steve Hull <hullstevenj@gmail.com>
2025-07-01 14:37:32 -0700

    Refactor getDashboard inportalController to pass belts to dashboard.pug

--- controllers/portalController.js
+++ controllers/portalController.js
@@ -1,6 +1,18 @@
 exports.getDashboard = (req, res, next) => {
     const user = res.locals.currentUser;
 
+    const belts = [
+      { name: 'Yellow Belt', slug: 'yellow' },
+      { name: 'Orange Belt', slug: 'orange' },
+      { name: 'Purple Belt', slug: 'purple' },
+      { name: 'Blue Belt', slug: 'blue' },
+      { name: 'Green Belt', slug: 'green' },
+      { name: 'Brown Belt', slug: 'brown' },
+      { name: 'Red Belt', slug: 'red' },
+      { name: 'Black/Red Belt', slug: 'blackred' },
+      { name: 'Black Belt', slug: 'black' }
+    ]
+
     const lastLoggedInFormatted = user?.lastLoggedIn
         ? new Date(user.lastLoggedIn).toLocaleString('en-us', {
             dateStyle: 'medium',
@@ -9,6 +21,7 @@ exports.getDashboard = (req, res, next) => {
         : 'N/A';
 
     res.render('portal/dashboard', { 
+        belts,
         pageTitle: 'Training Dashboard',
         currentPage: 'dashboard',
         user: res.locals.currentUser,
--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -4,13 +4,25 @@ const { Belt } = require('../models');
 const logger = require('../utils/loggerUtil');
 
 exports.getSection = async (req, res) => {
+    const belts = [
+      { name: 'Yellow Belt', slug: 'yellow' },
+      { name: 'Orange Belt', slug: 'orange' },
+      { name: 'Purple Belt', slug: 'purple' },
+      { name: 'Blue Belt', slug: 'blue' },
+      { name: 'Green Belt', slug: 'green' },
+      { name: 'Brown Belt', slug: 'brown' },
+      { name: 'Red Belt', slug: 'red' },
+      { name: 'Black/Red Belt', slug: 'blackred' },
+      { name: 'Black Belt', slug: 'black' }
+    ]
+
     const { beltColor, section } = req.params;
-    console.log(beltColor);
     const filePath = path.join(__dirname, '..', 'data', 'curriculum', beltColor, `${section}.json`);
 
     try {
         const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
         res.render(`training/${section}-template`, {
+            belts,
             beltColor,
             content: data
         });
