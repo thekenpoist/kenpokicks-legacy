52cb695d225ee9593da0c2eb3ff5a11df221b413
Steve Hull <hullstevenj@gmail.com>
2025-07-04 08:06:51 -0700

    Create beltModel

---
+++ models/beltModel.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Model, DataTypes, DATE } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Belt extends Model {
+        static associate(models) {
+        Belt.hasMany(models.Technique, { foreignKey: 'beltId' });
+        }
+
+        static async getAllOrdered() {
+            return await this.findAll({
+            order: [['beltRankOrder', 'ASC']],
+            });
+        }
+    }
+
+    Belt.init({
+        beltId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+        },
+        beltName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+        },
+        beltSlug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+        },
+        beltRankOrder: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+        },
+        beltNotes: {
+        type: DataTypes.TEXT,
+        allowNull: true
+        },
+        beltGroup: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+    }, {
+        sequelize,
+        modelName: 'Belt',
+        tableName: 'belts',
+        timestamps: true
+    });
+
+    return Belt;
+};
