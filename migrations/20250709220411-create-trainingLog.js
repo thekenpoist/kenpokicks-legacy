227a91c1255cda60d1b30b4710de1d869e5e1823
Steve Hull <hullstevenj@gmail.com>
2025-07-09 15:34:05 -0700

    Refactor users typo in trainingLog migration file

--- migrations/20250709220411-create-trainingLog.js
+++ migrations/20250709220411-create-trainingLog.js
@@ -11,7 +11,7 @@ module.exports = {
             type: Sequelize.UUID,
             allowNull: false,
             references: {
-              model: 'Users',
+              model: 'users',
               key: 'uuid'
             },
             onUpdate: 'CASCADE',
