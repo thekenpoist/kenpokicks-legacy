da1aebe4516fd0ec0591e0cc4430d6b27a4f282e
Steve Hull <hullstevenj@gmail.com>
2025-07-11 12:45:14 -0700

    Refactor login.pug, authRoutes, and userRoutes for CSRF prevention

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,23 +3,22 @@ const express = require('express');
 const { body } = require('express-validator');
 const { signupRules, loginRules } = require('../middleware/validators/authValidator');
 const processAvatar = require('../middleware/uploadAvatarMiddleware');
+const { csrfProtection, attachToken } = require('../middleware/csrfMiddleware');
 
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/signup', authController.getSignup);
-router.post('/signup', processAvatar, signupRules, authController.postSignup);
+router.get('/signup', csrfProtection, attachToken, authController.getSignup);
+router.post('/signup', processAvatar, csrfProtection, signupRules, authController.postSignup);
 
 router.get('/verify-email', authController.getVerifyEmail);
 
 router.get('/check-email', authController.getCheckEmail);
 
-router.get('/login', authController.getLogin);
-router.post('/login', loginRules, authController.postLogin);
+router.get('/login', csrfProtection, attachToken, authController.getLogin);
+router.post('/login', csrfProtection, loginRules, authController.postLogin);
 
 router.get('/logout', authController.getLogout);
-
-
 
 module.exports = router; 
