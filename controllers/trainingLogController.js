aa6da7ae0014c434861a116b7fefc70e050abbd3
Steve Hull <hullstevenj@gmail.com>
2025-07-12 17:37:52 -0700

    Create trainingLogController and map out CRUD functions

---
+++ controllers/trainingLogController.js
@@ -0,0 +1,40 @@
+const { validationResult } = require('express-validator');
+const { User, TrainingLog } = require('../models');
+const { DATE } = require('sequelize');
+const { renderServerError } = require('../utils/errorrUtil');
+const { formatInTimeZone } = require('date-fns-tz');
+const logger = require('../utils/loggerUtil');
+
+exports.getShowTrainingLogs = async (req, res, next) => {
+    const user = res.locals.currentUser;
+
+    if (!user) {
+        return res.redirect('/auth/login');
+    }
+}
+
+
+exports.getTrainingLog = async (req, res, next) => {
+
+}
+
+exports.getCreateTrainingLog = (req, res, next) => {
+
+}
+
+exports.postCreateTrainingLog = async (req, res, next) => {
+
+}
+
+exports.getEditTrainingLog = async (req, res, next) => {
+
+}
+
+exports.postEcitTrainingLog = async (req, RegExp, next) => {
+
+}
+
+exports.deleteTrainingLog = async (req, res, next) => {
+
+}
+
