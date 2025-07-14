69c21fc7b9a67cd8dae908f6b8dce63b7758e213
Steve Hull <hullstevenj@gmail.com>
2025-07-14 14:52:51 -0700

    Create getCreateTrainingLog in trainingLogController

--- controllers/trainingLogController.js
+++ controllers/trainingLogController.js
@@ -5,6 +5,29 @@ const { renderServerError } = require('../utils/errorrUtil');
 const { formatInTimeZone } = require('date-fns-tz');
 const logger = require('../utils/loggerUtil');
 
+
+exports.getCreateTrainingLog = (req, res, next) => {
+    res.render('logs/log-form', {
+        pageTitle: 'Create New Training Log',
+        currentPage: 'logs',
+        formAction: '/logs',
+        submitButtonText: 'Create Entry',
+        errorMessage: null,
+        crsfToken: req.crsfToken,
+        formData: {
+            user: req.locals.currentUser || ''
+        }
+    });
+};
+
+exports.postCreateTrainingLog = async (req, res, next) => {
+
+}
+
+
+
+
+
 exports.getShowTrainingLogs = async (req, res, next) => {
     const user = res.locals.currentUser;
 
@@ -18,13 +41,11 @@ exports.getTrainingLog = async (req, res, next) => {
 
 }
 
-exports.getCreateTrainingLog = (req, res, next) => {
+exports.getAllTrainingLogs = async (req, res, next) => {
 
 }
 
-exports.postCreateTrainingLog = async (req, res, next) => {
 
-}
 
 exports.getEditTrainingLog = async (req, res, next) => {
 
