4b3a785d978d2c22a37a0471760fe6d8e5cb3229
Steve Hull <hullstevenj@gmail.com>
2025-08-13 14:35:09 -0700

    Create getAllTechniques function in techniqueController

--- controllers/techniqueController.js
+++ controllers/techniqueController.js
@@ -39,3 +39,37 @@ exports.getBeltTechniques = async (req, res, next) => {
         return renderServerError(res, err, 'portal/dashboard');
     }
 };
+
+exports.getAllTechniques = async (req, res, next) => {
+    const user = res.locals.currentUser;
+    
+    if (!user) {
+        return res.redirect('/auth/login')
+    }
+
+    try {
+        const techniques = await Technique.findAll;
+
+        if (techniques.length === 0) {
+            return res.status(404).render('404', { 
+                    pageTitle: "Techniques not found",
+                    currentPage: 'portal/dashboard'
+                });
+        }
+
+        res.render('techniques', {
+            pageTitle: 'All Techniques',
+            currentPage: 'techniques',
+            layout: 'layouts/dashboard-layout',
+            errorMessage: null,
+            techniques
+        });
+    } catch (err) {
+        logger.error(`Error fetching techniques: ${err.message}`);
+        if (err.stack) {
+            logger.error(err.stack);
+        }
+
+        return renderServerError(res, err, 'portal/dashboard');
+    }
+}
