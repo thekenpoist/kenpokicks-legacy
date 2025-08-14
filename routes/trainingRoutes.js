26e3ef1b48b204055888d16e98aaac77f5db5116
Steve Hull <hullstevenj@gmail.com>
2025-08-14 09:07:18 -0700

    Refactor trainingRoutes for correct getBeltTechniques route

--- routes/trainingRoutes.js
+++ routes/trainingRoutes.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const express = require('express');
 const trainingController = require('../controllers/trainingController');
-const techniqueController = require('../controllers/techniqueController')
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
 
 const router = express.Router();
 
-router.get('/belt/:beltColor', isAuthenticated, techniqueController.getBeltTechniques);
+router.get('/belt/:beltColor', isAuthenticated, trainingController.getBeltTechniques);
 
 router.get('/:beltColor/:section', isAuthenticated, trainingController.getSection);
 
