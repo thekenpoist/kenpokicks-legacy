7f632e7557e7a678dbe76f4e7bb633332dbaea8c
Steve Hull <hullstevenj@gmail.com>
2025-08-14 08:59:52 -0700

    Move getBeltTechniques from techniqueController to trainingController

--- controllers/techniqueController.js
+++ controllers/techniqueController.js
@@ -3,42 +3,6 @@ const { Op } = require('sequelize');
 const { renderServerError } = require('../utils/errorrUtil')
 const logger = require('../utils/loggerUtil');
 
-exports.getBeltTechniques = async (req, res, next) => {
-    const beltColor = req.params.beltColor;
-    const user = res.locals.currentUser;
-
-    if (!user) {
-        return res.redirect('/auth/login');
-    }
-
-    try {
-        const techniques = await Technique.findAll({
-            where: { beltColor }
-        });
-
-        if (techniques.length === 0) {
-            return res.status(404).render('404', { 
-                pageTitle: "Techniques not found",
-                currentPage: 'portal/dashboard'
-            });
-        }
-
-        res.render('training/techniques-template', {
-            pageTitle: `Techniques for ${beltColor}`,
-            currentPage: 'techniques',
-            layout: 'layouts/dashboard-layout',
-            errorMessage: null,
-            techniques
-        });
-    } catch (err) {
-        logger.error(`Error fetching techniques: ${err.message}`);
-        if (err.stack) {
-            logger.error(err.stack);
-        }
-
-        return renderServerError(res, err, 'portal/dashboard');
-    }
-};
 
 exports.getAllTechniques = async (req, res, next) => {
     const user = res.locals.currentUser;
--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const logger = require('../utils/loggerUtil');
+const { Technique } = require('../models');
+
 
 exports.getSection = async (req, res) => {
     const { beltColor, section } = req.params;
@@ -18,5 +20,42 @@ exports.getSection = async (req, res) => {
             logger.error(err.stack);
         }
         res.status(404).render('404', { message: 'Section not found.'});
+    }
+};
+
+exports.getBeltTechniques = async (req, res, next) => {
+    const beltColor = req.params.beltColor;
+    const user = res.locals.currentUser;
+
+    if (!user) {
+        return res.redirect('/auth/login');
+    }
+
+    try {
+        const techniques = await Technique.findAll({
+            where: { beltColor }
+        });
+
+        if (techniques.length === 0) {
+            return res.status(404).render('404', { 
+                pageTitle: "Techniques not found",
+                currentPage: 'portal/dashboard'
+            });
+        }
+
+        res.render('training/techniques-template', {
+            pageTitle: `Techniques for ${beltColor}`,
+            currentPage: 'techniques',
+            layout: 'layouts/dashboard-layout',
+            errorMessage: null,
+            techniques
+        });
+    } catch (err) {
+        logger.error(`Error fetching techniques: ${err.message}`);
+        if (err.stack) {
+            logger.error(err.stack);
+        }
+
+        return renderServerError(res, err, 'portal/dashboard');
     }
 };
