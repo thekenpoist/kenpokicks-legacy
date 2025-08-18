39001663eb4b253434fb7542b31f105ff5809fcf
Steve Hull <hullstevenj@gmail.com>
2025-08-18 10:08:19 -0700

    Refactor getAdminConsole in adminController for creating user count

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,31 @@
-exports.getAdminConsole = (req, res, next) => {
+const { User } = require('../models');
+
+
+exports.getAdminConsole = async (req, res, next) => {
     const user = res.locals.currentUser;
 
     if (!user) {
         return res.redirect('/auth/login');
     }
+
+    let student = 0;
+    let instructor = 0;
+    let admin = 0;
+
+    const users = await User.findAll();
+
+    users.forEach(user => {
+        if (user.role === 'admin') { 
+            admin++;
+            instructor++;
+            student++;    
+        } else if (user.role === 'instructor') {
+            instructor++;
+            student++;
+        } else if (user.role === 'student') {
+            student++;
+        }
+    });
 
     if (user.role !== 'admin') {
         return res.status(403).render('403', {
