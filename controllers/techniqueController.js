0c49a730abd050d5b1adce19cc9b466055b4f1b5
Steve Hull <hullstevenj@gmail.com>
2025-08-27 15:59:01 -0700

    Finish out postEditTechnique in techniqueController

--- controllers/techniqueController.js
+++ controllers/techniqueController.js
@@ -1,4 +1,5 @@
 const { Technique } = require('../models');
+const { validationResult } = require('express-validator');
 const { Op } = require('sequelize');
 const { renderServerError } = require('../utils/errorrUtil')
 const logger = require('../utils/loggerUtil');
@@ -81,8 +82,75 @@ exports.getEditTechnique = async (req, res, next) => {
 
 exports.postEditTechnique = async (req, res, next) => {
     const user = res.locals.currentUser;
-
     if (!user) {
         return res.redirect('/auth/login');
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+
+        return res.status(422).render('techniques/tech-form', {
+            pageTitle: 'Edit Technique',
+            currentPage: 'techniques',
+            formAction: '/techniques',
+            submitButtonText: 'Edit Technique',
+            errorMessage: errors.array().map(e => e.msg).join(', '),
+            formData: req.body
+        });
+    }
+
+    const {
+        techTitle,
+        techSlug,
+        techAttack,
+        techDescription,
+        techGroup,
+        techAttackAngle,
+        techNotes,
+        relatedForm,
+        beltColor,
+        videoUrl,
+        lastUpdateBy
+    } = req.body;
+    
+    const { techId } = req.params;
+    try {
+        const technique = await Technique.findOne({
+            where: { techId }
+        });
+
+        if (!technique) {
+            return res.status(404).render('404', {
+                pageTitle: 'Technique Not Found',
+                currentPage: 'techniques'
+            });
+        }
+
+        await Technique.update({
+            techTitle,
+            techSlug,
+            techAttack,
+            techDescription,
+            techGroup,
+            techAttackAngle,
+            techNotes,
+            relatedForm,
+            beltColor,
+            videoUrl,
+            lastUpdateBy
+        }, {
+            where: { techId }
+        });
+    } catch (err) {
+        logger.error(`Error updating user: ${err.message}`);
+        if (err.stack) {
+            logger.error(err.stack);
+        }
+        res.status(500).render('techniques/tech-form', {
+            pageTitle: 'Edit Technique',
+            currentPage: 'techniques',
+            errorMessage: 'Something went wrong. Please try again.',
+            formData: req.body
+        });
     }
 }
