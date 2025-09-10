53c3b91b7281654e54d26cddec20a9b247168034
Steve Hull <hullstevenj@gmail.com>
2025-09-10 16:05:32 -0700

    Create middleware requireRoleMiddleware for verifying access to restricted pages

---
+++ middleware/requireRoleMiddleware.js
@@ -0,0 +1,23 @@
+module.exports = function requireRole(...allowed) {
+    return (req, res, next) => {
+        const user = res.locals.currentUser || req.user ||null;
+
+        if (!user) {
+            if (req.accepts('html')) return res.redirect('/auth/login');
+            return res.status(401).json({ error: 'Not Authenticated' });
+        }
+
+        if (!allowed.includes(user.role)) {
+            if (req.accepts('html')) {
+                return res.status(403).render('403', {
+                    pageTitle: 'Access Denied',
+                    currentPage: 'portal/dashboard',
+                    layout: 'layouts/dashboard-layout'
+                });
+            }
+            return res.status(403).json({ error: 'Forbidden' });
+        }
+
+        next();
+    };
+}
