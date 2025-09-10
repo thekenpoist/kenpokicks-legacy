e3ea1b232b0ea9feaf6306e48c81448f4e63e295
Steve Hull <hullstevenj@gmail.com>
2025-09-10 16:20:38 -0700

    Refactor techniqueRoutes for requireRoleMiddleware

--- routes/techniqueRoutes.js
+++ routes/techniqueRoutes.js
@@ -2,12 +2,13 @@ const path = require('path');
 const express = require('express');
 const techniqueController = require('../controllers/techniqueController');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
+const { requireRole } = require('../middleware/requireRoleMiddleware');
 
 const router = express.Router();
 
-router.get('/all', isAuthenticated, techniqueController.getAllTechniques)
+router.get('/all', isAuthenticated, requireRole('admin', 'instructor'), techniqueController.getAllTechniques)
 
-router.get('/:techId/edit', isAuthenticated, techniqueController.getEditTechnique);
-router.post('/:techId/edit', isAuthenticated, techniqueController.postEditTechnique)
+router.get('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.getEditTechnique);
+router.post('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.postEditTechnique)
 
 module.exports = router;
