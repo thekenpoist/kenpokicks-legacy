88a20aad9fe9c23d84c70636ebc4ccfc6f2f8b71
Steve Hull <hullstevenj@gmail.com>
2025-09-11 15:04:57 -0700

    Refactor techniqueRoutes for correct use of requireRole

--- routes/techniqueRoutes.js
+++ routes/techniqueRoutes.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 const techniqueController = require('../controllers/techniqueController');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
-const { requireRole } = require('../middleware/requireRoleMiddleware');
+const requireRole = require('../middleware/requireRoleMiddleware');
 
 const router = express.Router();
 
-router.get('/all', isAuthenticated, requireRole('admin', 'instructor'), techniqueController.getAllTechniques)
+router.get('/all', isAuthenticated, requireRole('admin'), techniqueController.getAllTechniques)
 
 router.get('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.getEditTechnique);
 router.post('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.postEditTechnique)
