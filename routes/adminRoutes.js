eacfe9f1bb4f74ea0da51b430cd972b76e971545
Steve Hull <hullstevenj@gmail.com>
2025-09-11 15:04:11 -0700

    Refactor adminRoutes for correct use of requireRole

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const adminController = require('../controllers/adminController');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
-const { requireRole } = require('../middleware/requireRoleMiddleware');
+const requireRole = require('../middleware/requireRoleMiddleware');
 
 const router = express.Router();
-
 
 router.get('/', isAuthenticated, requireRole('admin'), adminController.getAdminConsole);
 
