0fcbdd622c5aee46ceb93e8069b0c44b5177ba70
Steve Hull <hullstevenj@gmail.com>
2025-09-12 15:51:05 -0700

    Refactor authRoutes for CSRF implementation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,20 +3,21 @@ const express = require('express');
 const { body } = require('express-validator');
 const { signupRules, loginRules } = require('../middleware/validators/authValidator');
 const processAvatar = require('../middleware/uploadAvatarMiddleware');
+const { verifyCsrfToken } = require('../middleware/csrfMiddleware');
 
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', processAvatar, signupRules, authController.postSignup);
+router.post('/signup', processAvatar, verifyCsrfToken, signupRules, authController.postSignup);
 
 router.get('/verify-email', authController.getVerifyEmail);
 
 router.get('/check-email', authController.getCheckEmail);
 
 router.get('/login', authController.getLogin);
-router.post('/login', loginRules, authController.postLogin);
+router.post('/login', loginRules, verifyCsrfToken, authController.postLogin);
 
 router.get('/logout', authController.getLogout);
 
