68fd285cc12bea195bed14b6390604a14a4144d8
Steve Hull <hullstevenj@gmail.com>
2025-09-12 15:55:25 -0700

    Refactor userRoutes for CSRF implementation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,15 @@ const { body } = require('express-validator');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
 const { validateProfileUpdate } = require('../middleware/validators/userValidator')
 const processAvatar = require('../middleware/uploadAvatarMiddleware');
+const { verifyCsrfToken } = require('../middleware/csrfMiddleware');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/edit-profile', isAuthenticated, userController.getEditProfile);
-router.post('/edit-profile', isAuthenticated, processAvatar, validateProfileUpdate, userController.postEditProfile);
+router.post('/edit-profile', isAuthenticated, processAvatar, verifyCsrfToken, validateProfileUpdate, userController.postEditProfile);
 
-router.post('/delete-profile', isAuthenticated, userController.deleteProfile);
+router.post('/delete-profile', isAuthenticated, verifyCsrfToken, userController.deleteProfile);
 
 router.get('/me', userController.getShowProfile);
 
