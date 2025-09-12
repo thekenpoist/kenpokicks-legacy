de6beb54580236f8370b98c8b99dbbd7f54030ed
Steve Hull <hullstevenj@gmail.com>
2025-09-12 15:53:57 -0700

    Refactor trainingLogRoutes for CSRF implementation

--- routes/trainingLogRoutes.js
+++ routes/trainingLogRoutes.js
@@ -5,12 +5,13 @@ const { isAuthenticated } = require('../middleware/auth/authMiddleware');
 const { body } = require('express-validator');
 const { createTrainingLogRules } = require('../middleware/validators/trainingLogvalidator');
 const trainingLogController = require('../controllers/trainingLogController');
+const { verifyCsrfToken } = require('../middleware/csrfMiddleware');
 const upload = multer();
 const router = express.Router();
 
 // Create
 router.get('/new', isAuthenticated, trainingLogController.getCreateTrainingLog);
-router.post('/', isAuthenticated, upload.none(), createTrainingLogRules, trainingLogController.postCreateTrainingLog);
+router.post('/', isAuthenticated, upload.none(), verifyCsrfToken, createTrainingLogRules, trainingLogController.postCreateTrainingLog);
 
 // Read
 router.get('/all', isAuthenticated, trainingLogController.getAllTrainingLogs)
@@ -18,10 +19,10 @@ router.get('/recent', isAuthenticated, trainingLogController.getRecentTrainingLo
 
 // Update
 router.get('/edit/:logId', isAuthenticated, trainingLogController.getEditTrainingLog);
-router.post('/edit/:logId', isAuthenticated, upload.none(), createTrainingLogRules, trainingLogController.postEditTrainingLog);
+router.post('/edit/:logId', isAuthenticated, upload.none(), verifyCsrfToken, createTrainingLogRules, trainingLogController.postEditTrainingLog);
 
 // Delete
-router.post('/delete/:logId', isAuthenticated, trainingLogController.deleteTrainingLog);
+router.post('/delete/:logId', isAuthenticated, verifyCsrfToken, trainingLogController.deleteTrainingLog);
 
 // Read one log
 router.get('/:logId', isAuthenticated, trainingLogController.getOneTrainingLog);
