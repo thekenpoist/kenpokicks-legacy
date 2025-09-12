fe2eda853ea84ca4a888fc112772238b2fe025d5
Steve Hull <hullstevenj@gmail.com>
2025-09-12 15:52:18 -0700

    Refactor techniqueRoutes for CSRF implementation

--- routes/techniqueRoutes.js
+++ routes/techniqueRoutes.js
@@ -3,12 +3,13 @@ const express = require('express');
 const techniqueController = require('../controllers/techniqueController');
 const { isAuthenticated } = require('../middleware/auth/authMiddleware');
 const requireRole = require('../middleware/requireRoleMiddleware');
+const { verifyCsrfToken } = require('../middleware/csrfMiddleware');
 
 const router = express.Router();
 
 router.get('/all', isAuthenticated, requireRole('admin'), techniqueController.getAllTechniques)
 
 router.get('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.getEditTechnique);
-router.post('/:techId/edit', isAuthenticated, requireRole('admin'), techniqueController.postEditTechnique)
+router.post('/:techId/edit', isAuthenticated, verifyCsrfToken, requireRole('admin'), techniqueController.postEditTechnique)
 
 module.exports = router;
