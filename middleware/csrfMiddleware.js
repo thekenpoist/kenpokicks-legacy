1451abfb84e3f90e5cf511771a92c286a384ec3c
Steve Hull <hullstevenj@gmail.com>
2025-09-12 13:54:25 -0700

    Create verifyCsrfToken in csrfMiddleware

--- middleware/csrfMiddleware.js
+++ middleware/csrfMiddleware.js
@@ -8,4 +8,12 @@ function createCsrfToken(req, res, next) {
     next()
 }
 
-module.exports = createCsrfToken;
+function verifyCsrfToken(req, res, next) {
+    const token = req.body._csrf || req.headers['x-csrf-token'];
+    if (token && token === req.session.csrfToken) {
+        return next();
+    }
+    res.status(403).send('Invalid CSRF token');
+}
+
+module.exports = { createCsrfToken, verifyCsrfToken };
