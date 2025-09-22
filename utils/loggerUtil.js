7aa4c28785a2b9173536439680d4172b2780cc98
Steve Hull <hullstevenj@gmail.com>
2025-09-22 10:50:10 -0700

    Refactor loggerUtils for various typos

--- utils/loggerUtil.js
+++ utils/loggerUtil.js
@@ -6,11 +6,11 @@ const logDir = path.join(__dirname, '..', 'logs');
 const logger = createLogger({
     level: 'info',
     format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD:mm:ss' }),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         format.printf(info => `[${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`)
     ),
     transports: [
-        new transports.File({ filename: path.join(logDir, 'error.log'), level: 'error '}),
+        new transports.File({ filename: path.join(logDir, 'error.log'), level: 'error'}),
         new transports.File({ filename: path.join(logDir, 'combined.log')})
     ]
 });
@@ -20,7 +20,7 @@ logger.add(new transports.Console({
         format.colorize(),
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         format.printf(({ level, message, timestamp }) => {
-            return `[${timestamp}] ${level}: ${message}`;
+            return `[${timestamp}] ${level}: ${message}]`;
         })
     ),
     silent: true
