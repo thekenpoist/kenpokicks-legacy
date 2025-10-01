f3852a50c0fdaec9f04d82f191ee7a71b219a493
Steve Hull <hullstevenj@gmail.com>
2025-10-01 16:23:33 -0700

    Refactor posteEditUser route in adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,18 +4,16 @@ const { isAuthenticated } = require('../middleware/auth/authMiddleware');
 const requireRole = require('../middleware/requireRoleMiddleware');
 const processAvatar = require('../middleware/uploadAvatarMiddleware');
 const { verifyCsrfToken } = require('../middleware/csrfMiddleware');
+const { validateUserUpdate } = require('../middleware/validators/adminValidator');
 
 const router = express.Router();
-
-router.use((req, _res, next) => { console.log('[adminRouter]', req.method, req.originalUrl); next(); });
-
 
 router.get('/', isAuthenticated, requireRole('admin'), adminController.getAdminConsole);
 
 router.get('/all', isAuthenticated, requireRole('admin'), adminController.getAllUsers)
 
 router.get('/users/:uuid/edit', isAuthenticated, requireRole('admin'), adminController.getEditUser);
-router.post('/users/:uuid/update', isAuthenticated, processAvatar, verifyCsrfToken, requireRole('admin'), adminController.postEditUser);
+router.post('/users/:uuid/update', isAuthenticated, processAvatar, verifyCsrfToken, validateUserUpdate, requireRole('admin'), adminController.postEditUser);
 
 
 module.exports = router;
