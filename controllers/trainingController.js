56aa2981674b15a9f194a6ead8310374855c2b26
Steve Hull <hullstevenj@gmail.com>
2025-10-16 15:10:09 -0700

    Rename getSection in trainingController to getBeltCurriculum

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -4,7 +4,7 @@ const { logger } = require('../utils/loggerUtil');
 const { Technique } = require('../models');
 
 
-exports.getSection = async (req, res) => {
+exports.getBeltCurriculum = async (req, res) => {
     const { beltColor, section } = req.params;
     const ALLOWED_SECTIONS = new Set(['techniques', 'basics', 'forms', 'sets']);
     if (!ALLOWED_SECTIONS.has(section)) {
@@ -73,5 +73,40 @@ exports.getBeltTechniques = async (req, res, next) => {
         }
 
         return renderServerError(res, err, 'portal/dashboard');
+    }
+};
+
+exports.getAdvancedForms = async (req, res) => {
+    const { beltColor, section } = req.params;
+    const ALLOWED_SECTIONS = new Set(['techniques', 'basics', 'forms', 'sets']);
+    if (!ALLOWED_SECTIONS.has(section)) {
+        return res.status(404).render('404', { message: 'Section not found.' });
+    }
+
+    const filePath = path.join(__dirname, '..', 'data', 'curriculum', beltColor, `${beltColor}_${section}.json`);
+
+    try {
+        const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        res.render(`training/${section}-template`, {
+            beltColor,
+            section,
+            content: typeof data === 'object' && !Array.isArray(data) ? data : { [section]: data },
+            hasData: Array.isArray(data) ? data.length > 0 : !!data
+        });
+    } catch (err) {
+        if (err.code === 'ENOENT' && ALLOWED_SECTIONS.has(section)) {
+            return res.render(`training/${section}-template`, { 
+                beltColor,
+                section,
+                content: { [section]: [] },
+                hasData: false
+        })
+        }
+        logger.error(`Error loading curriculum for ${beltColor}/${section}: ${err.message}`);
+        if (err.stack) {
+            logger.error(err.stack);
+        }
+        res.status(ALLOWED_SECTIONS.has(section) ? 500 : 404)
+            .render(ALLOWED_SECTIONS.has(section) ? '500' : '404', { message: 'Problem loading this section.'});
     }
 };
