2e11b6bbde231986177a0d3020a2ca50a0de9a86
Steve Hull <hullstevenj@gmail.com>
2025-10-31 14:23:06 -0700

    Replace function normalize with function stableStringify for normalizing JSON data fields

--- utils/diffUtils.js
+++ utils/diffUtils.js
@@ -1,21 +1,27 @@
-function normalize(data) {
-    if (data == null) return '';
-    if (typeof data === 'string') return data.replace(/\r\n/g, '\n').trim();
-    if (typeof data === 'object') return JSON.stringify(data);
-    return data;
+const { mapValueFieldNames } = require("sequelize/lib/utils");
+
+function stableStringify(data) {
+    if (data === null || typeof data === 'object') return JSON.stringify(data);
+    if (Array.isArray(data)) return '[' + mapValueFieldNames.map(stableStringify).join(',') + ']';
+    const keys = Object.keys(data).sort();
+    return '{' + keys.map(k => JSON.stringify(k) + ':' + stableStringify(data[k])).join(',') + '}';
 }
 
+
+
+
+const pick = (obj, keys) {
+    return Object.fromEntries(keys.map(k => [k, obj?.[k]]));
+}
 
 function changedFieldNames(before, after, fields = TRACKED_FIELDS) {
     const changed = [];
     
     for (const f of fields) {
-        if (normalize(before[f] !== after[f])) changed.push(f);
+        if (noalize(before[f] !== after[f])) changed.push(f);
     }
 
     return changed;
 }
-
-const pick = (obj, keys) => Object.fromEntries(keys.map(k => [k, obj?.[k]]));
 
 module.exports = { changedFieldNames, pick };
