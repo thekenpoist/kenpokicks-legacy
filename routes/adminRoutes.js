e1cf1ed6cf91d70b096b9fd9e7d3ddac4f7398ec
Steve Hull <hullstevenj@gmail.com>
2025-11-10 15:24:31 -0800

    Refactor routes in adminRoutes for 'superadmin'

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -10,15 +10,15 @@ const router = express.Router();
 
 router.get('/', isAuthenticated, requireRole('admin', 'superadmin'), adminController.getAdminConsole);
 
-router.get('/all', isAuthenticated, requireRole('admin'), adminController.getAllUsers)
+router.get('/all', isAuthenticated, requireRole('admin', 'superadmin'), adminController.getAllUsers)
 
-router.get('/users/:uuid/show', isAuthenticated, requireRole('admin'), adminController.getOneUser);
+router.get('/users/:uuid/show', isAuthenticated, requireRole('admin', 'suerpadmin'), adminController.getOneUser);
 
-router.get('/logs/recent', isAuthenticated, requireRole('admin'), adminController.getRecentAdminLogs);
-router.get('/logs', isAuthenticated, requireRole('admin'), adminController.getAllAdminLogs);
+router.get('/logs/recent', isAuthenticated, requireRole('admin', 'superadmin'), adminController.getRecentAdminLogs);
+router.get('/logs', isAuthenticated, requireRole('admin', 'superadmin'), adminController.getAllAdminLogs);
 
-router.get('/users/:uuid/edit', isAuthenticated, requireRole('admin'), adminController.getEditUser);
-router.post('/users/:uuid/update', isAuthenticated, processAvatar, verifyCsrfToken, validateUserUpdate, requireRole('admin'), adminController.postEditUser);
+router.get('/users/:uuid/edit', isAuthenticated, requireRole('admin', 'superadmin'), adminController.getEditUser);
+router.post('/users/:uuid/update', isAuthenticated, processAvatar, verifyCsrfToken, validateUserUpdate, requireRole('admin', 'superadmin'), adminController.postEditUser);
 
 
 module.exports = router;
